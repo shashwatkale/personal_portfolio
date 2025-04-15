cefcc062b5fe8e1cb2e1d7356dad5b3a5f917a4c
shashwatkale <shashwat.kale.27@gmail.com>
2025-04-15 15:56:04 +0530

    Optimized assets and code-split for performance

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/personal_portfolio/',
-})
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            const directories = id.toString().split('node_modules/')[1].split('/');
+            if (directories[0].startsWith('@')) {
+              return `${directories[0]}/${directories[1]}`;
+            }
+            return directories[0];
+          }
+        },
+      },
+    },
+  },
+});
